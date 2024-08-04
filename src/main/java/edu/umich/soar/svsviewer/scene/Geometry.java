ef790a43e2e631c6b431efcd4c1336c2558122c0
Nathan Glenn <garfieldnate@gmail.com>
2024-08-03 19:43:57 -0500

    Style labels for readability
    Because black on black objects is impossible to see!

--- src/main/java/edu/umich/soar/svsviewer/scene/Geometry.java
+++ src/main/java/edu/umich/soar/svsviewer/scene/Geometry.java
@@ -2,7 +2,9 @@ package edu.umich.soar.svsviewer.scene;
 
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 // The contained group may have 0 or 1 children; the 1 child would be a Shape3d.
 // We use a Group because we need to be able to specify position/rotation/scale
@@ -12,27 +14,17 @@ public class Geometry {
   private final String name;
   private final Group group;
 
-  private final Group label;
+  private final TextFlow label;
 
   public Geometry(String name, SVSScene parent) {
     this.name = name;
     this.parent = parent;
     group = new Group();
-    // wrap in Group so we can choose the exact location in the pane
-    label = new Group(new Text(name));
-    //    SAD! This doesn't work. Need bounds on scene or screen, which there is no listener for.
-    // These don't update when the root group rotates or the camera moves, etc.
-    group
-        //        .boundsInLocalProperty()
-        .boundsInParentProperty()
-        .addListener(
-            (observable, oldValue, newValue) ->
-                // TODO NEXT: add a label object and update its location here.
-                System.out.println(
-                    "Geometry "
-                        + this.name
-                        + " updated; screen location is "
-                        + group.localToScreen(0, 0, 0)));
+    // Text doesn't support background color, so wrap in TextFlow, which does
+    label = new TextFlow(new Text(name));
+    // black text on white background for readability
+    label.setStyle(
+        "-fx-background-color: white; -fx-font-weight: bold; -fx-font-family: 'Helvetica'");
   }
 
   public Group getGroup() {
