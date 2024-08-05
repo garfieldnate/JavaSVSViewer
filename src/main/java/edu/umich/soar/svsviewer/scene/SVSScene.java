b06e481b1c3511c33fbeab15235e693fe1a8923f
Nathan Glenn <garfieldnate@gmail.com>
2024-08-05 18:25:32 -0500

    remove dead code

--- src/main/java/edu/umich/soar/svsviewer/scene/SVSScene.java
+++ src/main/java/edu/umich/soar/svsviewer/scene/SVSScene.java
@@ -4,11 +4,8 @@ import edu.umich.soar.svsviewer.util.WildcardMap;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 
-public record SVSScene(String name, WildcardMap<Geometry> geometries, Group root, Group labels) {
+public record SVSScene(String name, WildcardMap<Geometry> geometries, Group root) {
   public SVSScene(String name) {
-    this(name, new WildcardMap<>(), new Group(), new Group());
-    //    NEXT: figure out how to show this label properly in an overlay.
-    //      https://stackoverflow.com/questions/46011515/static-2d-text-over-3d-scene-in-javafx-java
-    labels().getChildren().add(new Label("hello"));
+    this(name, new WildcardMap<>(), new Group());
   }
 }
